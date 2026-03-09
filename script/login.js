98e2acb297b542f73af1b6c21d3c3fa895638d10
unknown <you@example.com>
2026-03-09 17:26:12 +0600

    Add dynamic border color based on issue status

--- script/login.js
+++ script/login.js
@@ -1,17 +1,14 @@
-document.getElementById("Sign-Btn").addEventListener("click",function(event){
+document.getElementById("Sign-Btn").addEventListener("click", function(event){
     event.preventDefault(); 
-    const userName = document.getElementById("user-name" ).value;
-    
 
+    const userName = document.getElementById("user-name").value;
     const password = document.getElementById("Password").value;
 
-     if(userName == "admin" && password == "admin123"){
-        alert("log in successful")
-
+    if(userName === "admin" && password === "admin123"){
+        alert("Login successful");
         window.location.assign("/home.html");
     }
     else{
         alert("Invalid username or password");
-        return;
     }
 });
